006176c05f65487251c769b8a7ab8c4e373b51c4
Chen <jonkiky@gmail.com>
2024-08-31 08:09:55 -0400

    integrated with google auth

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
 	env:
 	{
-		GOOGLE_CLIENT:"95758616682-pin4m2uuhgk37bbjens91c7t8o97dlvh.apps.googleusercontent.com",
-		GOOGLE_SECRET:"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"
+		GOOGLE_CLIENT_ID:"95758616682-pin4m2uuhgk37bbjens91c7t8o97dlvh.apps.googleusercontent.com",
+		GOOGLE_CLIENT_SECRET:"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"
 	}
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "@/css/satoshi.css";
 import "@/css/style.css";
 import React, { useEffect, useState } from "react";
 import Loader from "@/components/common/Loader";
+import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({
   children,
@@ -23,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
-        <div className="dark:bg-boxdark-2 dark:text-bodydark">
+          <div className="dark:bg-boxdark-2 dark:text-bodydark">
           {loading ? <Loader /> : children}
         </div>
       </body>
