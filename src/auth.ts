1aca481305ca8efd6748e77e84e955c7b076ed0a
Chen <jonkiky@gmail.com>
2024-09-07 07:49:30 -0400

    add session control

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,16 @@
 
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+import PostgresAdapter from "@auth/pg-adapter"
+import { Pool } from "@neondatabase/serverless"
+
+
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+  // Create a `Pool` inside the request handler.
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+  return {
+    adapter: PostgresAdapter(pool),
+    providers: [Google],
+  }
 })
+
